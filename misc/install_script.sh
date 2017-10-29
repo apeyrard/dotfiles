929d9e9a194e29d51a81e6b689ffeeb06b583c5b
Alex Peyrard <alex.peyrard@gmail.com>
2017-10-29 13:54:15 +0100

    Update install_script.sh

--- misc/install_script.sh
+++ misc/install_script.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 pacman -Sy archlinux-keyring
-
 pacman -Syu
+pacman -S zsh
 
+# adding user
 useradd -m -G wheel -s /bin/zsh alex
 passwd alex
 
 echo "Add yourself to sudoers"
 visudo
+
+# adding archlinuxfr and yaourt
 cp /etc/pacman.conf /etc/pacman.conf.back
 
 echo "Adding archlinuxfr"
@@ -19,8 +22,12 @@ echo "Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
 echo "Installing yaourt"
 pacman -Syu yaourt
 
+# cloning into dotfiles
 su alex
 cd
 echo "cloning into dotfiles"
 
+# linking config files
+
+# installing everything else
 yaourt -S xorg-server xorg-xinit wget bspwm compton sxhkd qterminal deluge feh base-devel polybar universal-ctags-git tmux rofi rfkill openssh
