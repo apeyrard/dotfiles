6431f3703629d95d5b1c98df651d9f688cc8ed8d
Alex Peyrard <alex.peyrard@gmail.com>
2017-10-29 14:25:12 +0100

    update install script

--- misc/install_script.sh
+++ misc/install_script.sh
@@ -30,6 +30,7 @@ su alex -c "git clone https://github.com/apeyrard/dotfiles.git"
 # linking config files
 su alex -c "ln -s /home/alex/dotfiles /home/alex/.config"
 su alex -c "ln -s /home/alex/dotfiles/misc/.xinitrc /home/alex/.xinitrc"
+su alex -c "ln -s /home/alex/dotfiles/misc/.profile /home/alex/.profile"
 
 # installing everything else
 su alex -c "yaourt -S xorg-server xorg-xinit wget bspwm compton sxhkd qterminal deluge feh base-devel polybar universal-ctags-git tmux rofi rfkill openssh"
