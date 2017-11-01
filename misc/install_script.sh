556460e5a9f1713b89ede170959e4f97a00386ea
Alex Peyrard <alex.peyrard@gmail.com>
2017-11-01 21:09:11 +0100

    Update install_script.sh

--- misc/install_script.sh
+++ misc/install_script.sh
@@ -39,5 +39,15 @@ su alex -c "ln -s /home/alex/.config/misc/.profile /home/alex/.profile"
 su alex -c "gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53"
 su alex -c "yaourt -S wireless_tools mpd libmpdclient alsa-lib siji-git ttf-unifont cower"
 
+# installing powerline fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 # installing everything else
 su alex -c "yaourt -S xorg-server xorg-xinit wget bspwm compton sxhkd qterminal deluge feh base-devel polybar universal-ctags-git tmux rofi rfkill openssh"
